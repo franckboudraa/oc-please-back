52b149611c6e930e9eee9913eb484df35bfa3684
neocydfr <franckboudraa@gmail.com>
2018-03-23 18:23:02 +0100

    improved authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_request
+  skip_before_action :authenticate_request, only: [:create]
 
   def create
     @user = User.new(email: params[:email], password: params[:password], first_name: params[:first_name], last_name: params[:last_name])
 
     if @user.valid? && @user.save
-      return render json: @user
+      token =  AuthenticateUser.call(params[:email], params[:password])
+      return render json: {token: token.result}
     else
       return render json: {error: 'Invalid params'}, status: :unauthorized
     end
+  end
+
+  def current
+    render json: @current_user.attributes.except("password_digest")
   end
 end
