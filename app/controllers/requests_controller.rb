13e23ebf5029bf07bc2b8d9a7a47a98bc2c855bf
neocydfr <franckboudraa@gmail.com>
2018-03-30 14:01:22 +0200

    returns request id after creation

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,11 +4,17 @@ class RequestsController < ApplicationController
   end
 
   def create
-    @request = Request.new(address: params[:address], title: params[:title], description: params[:description], lat: params[:lat], lng: params[:lng], reqtype: params[:reqtype])
+    @request = Request.new(address: params[:address],
+                           title: params[:title],
+                           description: params[:description],
+                           lat: params[:lat],
+                           lng: params[:lng],
+                           reqtype: params[:reqtype])
+
     @request.user_id = @current_user.id
 
     if @request.valid? && @request.save
-      return render status: 200
+      return render json: {id: @request.id}, status: 200
     else
       return render status: 400
     end
