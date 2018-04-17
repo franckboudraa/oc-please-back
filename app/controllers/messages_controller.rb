ae15fd427e82eaef926c109c9d60800ff1f0fb46
Franck Boudraa <franckboudraa@gmail.com>
2018-04-17 18:07:17 +0200

    added messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,23 @@
+class MessagesController < ApplicationController
+  before_action :volunteer_id_needed, only: [:create]
+
+  def create
+    @volunteer = Volunteer.find_by_id(params[:id])
+  # add user authorization check (request creator or helper?)
+    if @volunteer && @volunteer.user_id == @current_user.id || @volunteer && @volunteer.request.user_id == @current_user.id
+        @message = Message.new(user_id: @current_user.id, content: params[:content], volunteer_id: @volunteer.id)
+        if @message.valid? && @message.save
+          return render json:{request_id: @volunteer.request.id}, status: 201
+        else
+          return render status: 400
+        end
+    else
+      return render status: 404
+    end
+  end
+
+  private
+  def volunteer_id_needed
+    return render status: 400 unless params[:id]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   end
 
   resources :volunteers
+
+  resources :messages
 end
